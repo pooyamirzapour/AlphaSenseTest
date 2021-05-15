9964ca4c4b9bff3638f3b02dfb2f63ece9058d15
P.Mirzapour <pooyamirzapour@yahoo.com>
2021-05-15 19:45:08 +0430

    alpha sense

--- src/main/java/ContinuousSubArray.java
+++ src/main/java/ContinuousSubArray.java
@@ -1,16 +1,14 @@
 public class ContinuousSubArray {
 
-    public static void main(String[] args) {
-        int[] ints = new int[]{1, 2, 3, 4, 5, 8, 100, 1};
-         System.out.println(isExist(ints, 18));
-    }
-
     public static boolean isExist(int[] array, int target) {
         int sum = 0;
         int backIndex = 0;
         int frontIndex = 0;
         for (int index = 0; index <= array.length; index++) {
-            if (sum == target)
+
+            if (sum == target )
+                return true;
+            if ( index<= array.length-1 && array[index]==target)
                 return true;
             if (sum > target) {
                 index = backIndex + 1;
@@ -18,40 +16,13 @@ public class ContinuousSubArray {
                 backIndex++;
             } else {
                 sum = sum + array[frontIndex];
-                frontIndex=index+1;
-
+                frontIndex = index + 1;
+                if (frontIndex > array.length)
+                    return false;
             }
         }
         return false;
     }
 
-    public static boolean isExistWhile(int[] array, int target) {
-        int start = 0, end = 0;
 
-        int currSum = 0;
-
-        while (start < array.length && end <= array.length) {
-            if (currSum == target) {
-
-                if (end <= array.length - 1) {
-                    currSum += array[end];
-                }
-                end++;
-
-            } else {
-
-                if (currSum > target) {
-                    currSum -= array[start];
-                    start++;
-                } else {
-
-                    if (end <= array.length - 1) {
-                        currSum += array[end];
-                    }
-                    end++;
-                }
-            }
-        }
-        return true;
-    }
 }
---
+++ src/test/java/ContinuousSubArrayTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ContinuousSubArrayTest {
+
+    @Test
+    void isExistTest() {
+        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1},101));
+        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 7},7));
+        assertEquals(false, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4},8));
+        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1},108));
+//        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1},1));
+//        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1},13));
+//        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1},3));
+
+    }
+}

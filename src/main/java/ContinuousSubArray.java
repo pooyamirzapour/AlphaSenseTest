2a5547a59ae7dbf5f5b8670fc1d4c34ecff30160
P.Mirzapour <pooyamirzapour@yahoo.com>
2021-05-15 19:55:45 +0430

    alpha sense

--- src/main/java/ContinuousSubArray.java
+++ src/main/java/ContinuousSubArray.java
@@ -1,11 +1,14 @@
 public class ContinuousSubArray {
 
+    public static void main(String[] args) {
+        System.out.println(   isExist(new int[]{1, 2, 3, 4}, 8));
+    }
+
     public static boolean isExist(int[] array, int target) {
         int sum = 0;
         int backIndex = 0;
         int frontIndex = 0;
         for (int index = 0; index <= array.length; index++) {
-
             if (sum == target )
                 return true;
             if ( index<= array.length-1 && array[index]==target)

cdf6d531b140f1a53e97642efeda78fad826c54e
P.Mirzapour <pooyamirzapour@yahoo.com>
2021-05-16 09:43:01 +0430

    edge cases

--- src/main/java/ContinuousSubArray.java
+++ src/main/java/ContinuousSubArray.java
@@ -1,25 +1,33 @@
+import java.util.Arrays;
+
 public class ContinuousSubArray {
 
     public static void main(String[] args) {
-        System.out.println(   isExist(new int[]{1, 2, 3, 4}, 8));
+        System.out.println(isExist(new int[]{1, 2, 3, 4}, 8));
     }
 
     public static boolean isExist(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("The given array is not valid");
+        }
+        if (target == 0) {
+            return (Arrays.stream(array).anyMatch(p -> p == 0));
+        }
         int sum = 0;
         int backIndex = 0;
         int frontIndex = 0;
         for (int index = 0; index <= array.length; index++) {
-            if (sum == target )
+            if (sum == target)
                 return true;
-            if ( index<= array.length-1 && array[index]==target)
+            if (index <= array.length - 1 && array[index] == target)
                 return true;
             if (sum > target) {
                 index = backIndex + 1;
                 sum = sum - array[backIndex];
                 backIndex++;
             } else {
-                if (frontIndex<=array.length-1)
-                sum = sum + array[frontIndex];
+                if (frontIndex <= array.length - 1)
+                    sum = sum + array[frontIndex];
                 frontIndex = index + 1;
                 if (frontIndex > array.length)
                     return false;
--- src/test/java/ContinuousSubArrayTest.java
+++ src/test/java/ContinuousSubArrayTest.java
@@ -1,20 +1,23 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ContinuousSubArrayTest {
 
     @Test
     void isExistTest() {
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 101));
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 7}, 7));
-        assertEquals(false, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4}, 8));
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 108));
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 1));
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 13));
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 3));
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 150}, 150));
-        assertEquals(true, ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5}, 15));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 101));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 7}, 7));
+        assertFalse(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4}, 8));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 108));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 1));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 13));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 1}, 3));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5, 8, 100, 150}, 150));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5}, 15));
+        assertFalse(ContinuousSubArray.isExist(new int[]{1, 2, 3, 4, 5}, 0));
+        assertTrue(ContinuousSubArray.isExist(new int[]{1, 2, 3, 0, 5}, 0));
+        assertThrows(IllegalArgumentException.class, () -> ContinuousSubArray.isExist(null, 0));
 
     }
 }
